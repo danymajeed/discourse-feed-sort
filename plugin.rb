c20fa4a366f51467ba17e541d13494b4de6d8990
Daniyal Majeed <daniyalmajeedmirza@yahoo.com>
2021-04-24 03:51:11 +0500

    bypassed topic bump

--- plugin.rb
+++ plugin.rb
@@ -11,5 +11,44 @@ enabled_site_setting :feed_sort
 PLUGIN_NAME ||= 'FeedSort'
 
 after_initialize do
-  # https://github.com/discourse/discourse/blob/master/lib/plugin/instance.rb
+  class ::PostCreator
+        module UpdateTopicBump
+            def update_topic_stats
+                attrs = { updated_at: Time.now }
+                if @post.post_type != Post.types[:whisper] && !@opts[:silent]
+                    attrs[:last_posted_at] = @post.created_at
+                    attrs[:last_post_user_id] = @post.user_id
+                    attrs[:word_count] = (@topic.word_count || 0) + @post.word_count
+                    attrs[:excerpt] = @post.excerpt_for_topic if new_topic?
+                end
+                @topic.update_columns(attrs)
+            end
+        end
+        prepend UpdateTopicBump
+    end
+
+    class ::PostRevisor
+        module UpdateTopicBump
+            def bypass_bump?
+                return true
+            end
+        end
+        prepend UpdateTopicBump
+    end
+
+    class ::PostAction
+        module UpdateTopicBump
+            def update_counters
+                super()
+                # bumping topic on post like
+                bumped_at = Time.now()
+                topic_id = Post.with_deleted.where(id: post_id).pluck_first(:topic_id)
+                post = Post.where(id: post_id)
+                Topic.where(id: topic_id).update_all(
+                    bumped_at: bumped_at
+                )
+            end
+        end
+        prepend UpdateTopicBump
+    end
 end
