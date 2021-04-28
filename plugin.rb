baebc693006d8d213071225a0c94e36700721448
Daniyal Majeed <daniyalmajeedmirza@yahoo.com>
2021-04-28 19:24:03 +0500

    updated topic bump bypass

--- plugin.rb
+++ plugin.rb
@@ -20,6 +20,9 @@ after_initialize do
                     attrs[:last_post_user_id] = @post.user_id
                     attrs[:word_count] = (@topic.word_count || 0) + @post.word_count
                     attrs[:excerpt] = @post.excerpt_for_topic if new_topic?
+                    if @topic.created_at.to_time > Time.now - (3600 * SiteSetting.disable_bump_for)
+                      attrs[:bumped_at] = @post.created_at
+                    end
                 end
                 @topic.update_columns(attrs)
             end
@@ -36,19 +39,19 @@ after_initialize do
         prepend UpdateTopicBump
     end
 
-    class ::PostAction
-        module UpdateTopicBump
-            def update_counters
-                super()
-                # bumping topic on post like
-                bumped_at = Time.now()
-                topic_id = Post.with_deleted.where(id: post_id).pluck_first(:topic_id)
-                post = Post.where(id: post_id)
-                Topic.where(id: topic_id).update_all(
-                    bumped_at: bumped_at
-                )
-            end
-        end
-        prepend UpdateTopicBump
-    end
+    # class ::PostAction
+    #     module UpdateTopicBump
+    #         def update_counters
+    #             super()
+    #             # bumping topic on post like
+    #             bumped_at = Time.now()
+    #             topic_id = Post.with_deleted.where(id: post_id).pluck_first(:topic_id)
+    #             post = Post.where(id: post_id)
+    #             Topic.where(id: topic_id).update_all(
+    #                 bumped_at: bumped_at
+    #             )
+    #         end
+    #     end
+    #     prepend UpdateTopicBump
+    # end
 end
